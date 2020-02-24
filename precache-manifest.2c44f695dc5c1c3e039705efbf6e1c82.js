7426883751fd5959d88976e374d3c238cd7b0936
todovica <ana.todovic@gmail.com>
2020-02-24 15:38:31 +0100

    Updates

---
+++ precache-manifest.2c44f695dc5c1c3e039705efbf6e1c82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67fa9910470b4a48b7fd7c55bb339bfd",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "ac519d37759b3bcf580d",
+    "url": "/web/static/css/main.0b3051b3.chunk.css"
+  },
+  {
+    "revision": "c627fe9fcf377d77ec47",
+    "url": "/web/static/js/2.c3b9d598.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/web/static/js/2.c3b9d598.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac519d37759b3bcf580d",
+    "url": "/web/static/js/main.7c9d92ab.chunk.js"
+  },
+  {
+    "revision": "97f6d05a5e13ec09387e",
+    "url": "/web/static/js/runtime-main.6c8e0269.js"
+  },
+  {
+    "revision": "1b0f7a9b99d226269555cb27aeebbc73",
+    "url": "/web/static/media/logo.1b0f7a9b.png"
+  }
+]);
